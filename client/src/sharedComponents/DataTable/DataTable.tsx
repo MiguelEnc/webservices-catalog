fbe2e689957e7049916bbcd954e6bbf74c4e59c8
Miguel Encarnacion <miguelencarnacion7@gmail.com>
2021-03-22 23:55:38 -0400

    create DataTable shared component

---
+++ client/src/sharedComponents/DataTable/DataTable.tsx
@@ -0,0 +1,87 @@
+import React, { forwardRef } from 'react';
+import MaterialTable, { Action, Column, Icons, Localization, Options } from 'material-table';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Search from '@material-ui/icons/Search';
+
+export interface DataTableProps<RowData extends object> {
+    columns: Column<RowData>[];
+    data: RowData[];
+    editButton: boolean;
+    editFunc?: (event: any, data: RowData | RowData[]) => void;
+    pagination?: boolean;
+}
+
+export default function DataTable<RowData extends object>({
+    columns,
+    data,
+    editButton = false,
+    editFunc = (event: any, data: any) => console.log('Edit CLicked'),
+    pagination = true
+}: DataTableProps<RowData>) {
+    const tableIcons: Icons = {
+        Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+        Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+        DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+        FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+        LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+        NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+        PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+        ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+        Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+        SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />)
+    };
+
+    let actions: Action<RowData>[] = [];
+    if (editButton) {
+        actions.push({
+            icon: 'create',
+            onClick: editFunc
+        });
+    }
+
+    const options: Options<RowData> = {
+        actionsColumnIndex: -1,
+        pageSize: 5,
+        pageSizeOptions: [5, 10, 15, 20],
+        search: true,
+        paging: pagination,
+        draggable: false,
+        showTitle: false,
+        emptyRowsWhenPaging: false,
+        headerStyle: {
+            backgroundColor: 'rgba(249, 248, 245, 0.62)',
+            fontWeight: 500,
+            color: '#3d3d3d',
+            borderTop: 0,
+            cursor: 'pointer',
+            lineHeight: 1.42857143,
+            textAlign: 'left'
+        }
+    };
+
+    const localization: Localization = {
+        toolbar: {
+            searchTooltip: '',
+            searchPlaceholder: 'Search...'
+        }
+    };
+
+    return (
+        <div style={{ maxWidth: '100%' }}>
+            <MaterialTable
+                localization={localization}
+                actions={actions}
+                options={options}
+                columns={columns}
+                icons={tableIcons}
+                data={data}
+            />
+        </div>
+    );
+}
