b4f9d3c91e40438f09c26aba8d40d68c47930b39
Miguel Encarnacion <miguelencarnacion7@gmail.com>
2021-03-23 23:57:11 -0400

    add logging functions

---
+++ client/src/utility/Logger.ts
@@ -0,0 +1,24 @@
+const getTimeStamp = (): string => {
+    return new Date().toISOString();
+};
+
+const info = (namespace: string, message: string, object?: any) => {
+    if (object) {
+        console.info(`[${getTimeStamp()}] [INFO] [${namespace}] ${message}`, object);
+    } else {
+        console.info(`[${getTimeStamp()}] [INFO] [${namespace}] ${message}`);
+    }
+};
+
+const error = (namespace: string, message: string, object?: any) => {
+    if (object) {
+        console.error(`[${getTimeStamp()}] [ERROR] [${namespace}] ${message}`, object);
+    } else {
+        console.error(`[${getTimeStamp()}] [ERROR] [${namespace}] ${message}`);
+    }
+};
+
+export default {
+    info,
+    error
+};
